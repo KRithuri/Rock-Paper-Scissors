d55fe8d9b31d738b79b86a8a8acafa1850d3ee07
Karabo Rithuri <rithurikarabo@gmail.com>
2023-08-18 19:38:49 +0200

    display instructions to user

--- play.py
+++ play.py
@@ -1,2 +1,8 @@
 # import random module
 import random
+# print multiline instruction
+# performstring concatenation of string
+print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
+      + "Rock vs Paper -> Paper wins \n"
+      + "Rock vs Scissors -> Rock wins \n"
+      + "Paper vs Scissors -> Scissor wins \n")
