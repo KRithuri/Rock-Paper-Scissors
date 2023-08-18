4c20286bf7821c05594e612a46ccd3d7307a09e4
Karabo Rithuri <rithurikarabo@gmail.com>
2023-08-18 20:26:40 +0200

    computer choice via random module

--- game.py
+++ game.py
@@ -3,3 +3,4 @@ import random
 while True:
     player = input("Enter a choice (R-rock, P-paper, S-scissors): ")
     possible_actions = ["r","p","s"]
+    computer_choice = random.choice(possible_actions)
