d69523c0b0873dd4f53c590b0df700720bb2770a
Karabo Rithuri <rithurikarabo@gmail.com>
2023-08-18 19:40:00 +0200

    take input from user

--- play.py
+++ play.py
@@ -1,8 +1,12 @@
 # import random module
 import random
+
 # print multiline instruction
-# performstring concatenation of string
 print('Winning rules of the game ROCK PAPER SCISSORS are :\n'
       + "Rock vs Paper -> Paper wins \n"
       + "Rock vs Scissors -> Rock wins \n"
       + "Paper vs Scissors -> Scissor wins \n")
+while True:
+    print("Enter your choice \n 1 - Rock \n 2 - Paper \n 3 - Scissors \n")
+    # take the input from user
+    choice=int(input("Enter your choice :"))
