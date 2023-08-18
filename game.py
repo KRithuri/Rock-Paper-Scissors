2deda5a3512abe5afca11ebf7ffa1c71d6e64de9
Karabo Rithuri <rithurikarabo@gmail.com>
2023-08-18 20:27:38 +0200

    display choices to user

--- game.py
+++ game.py
@@ -4,3 +4,5 @@ while True:
     player = input("Enter a choice (R-rock, P-paper, S-scissors): ")
     possible_actions = ["r","p","s"]
     computer_choice = random.choice(possible_actions)
+    print(f"\nYou chose {player}, computer chose {computer_choice}.\n")
+    
